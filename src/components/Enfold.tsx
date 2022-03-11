87be4069de6bbbe611741b2b704b8e26c441265c
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-12 00:04:49 +0800

    add component

--- src/components/Enfold.tsx
+++ src/components/Enfold.tsx
@@ -2,7 +2,9 @@ import { FC } from "react";
 import { EnfoldProps } from "./Enfold.types";
 
 const Enfold: FC<EnfoldProps> = (props) => {
-  return <div></div>;
+  return <div>
+    
+  </div>;
 };
 
 export default Enfold;
