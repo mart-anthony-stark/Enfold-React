957ef5de88cc49d1969e0183aed85238ced4eb13
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-12 00:07:40 +0800

    add styles opts

--- src/components/Enfold.tsx
+++ src/components/Enfold.tsx
@@ -2,9 +2,16 @@ import { FC } from "react";
 import { EnfoldProps } from "./Enfold.types";
 
 const Enfold: FC<EnfoldProps> = (props) => {
-  return <div>
-    
-  </div>;
+  return (
+    <div
+      id="enfold"
+      style={{
+        animationDuration: props.options.duration,
+        animationTimingFunction: props.options.easing,
+        animationDelay: props.options.delay,
+      }}
+    ></div>
+  );
 };
 
 export default Enfold;
