fd525db54bc3d186b7841387005fc3b0f0b98f0f
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-11 21:04:21 +0800

    initial commit

---
+++ src/components/Enfold.types.ts
@@ -0,0 +1,11 @@
+import {} from "react";
+export interface EnfoldProps {
+  name: string;
+  options?: {
+    delay?: string;
+    duration?: string;
+    easing?: string;
+    intensity?: number;
+    threshold?: number;
+  };
+}
