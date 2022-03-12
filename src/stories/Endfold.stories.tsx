90e8a8465ab80eebb4a4949a698573aee785840d
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-12 21:22:40 +0800

    add children

--- src/stories/Endfold.stories.tsx
+++ src/stories/Endfold.stories.tsx
@@ -4,6 +4,8 @@ import { Story, Meta } from "@storybook/react";
 import Enfold from "../components/Enfold";
 import { EnfoldProps } from "../components/Enfold.types";
 
+import './enfold.css'
+
 export default {
   title: "Mart/Enfold",
   component: Enfold,
@@ -14,5 +16,6 @@ const Template: Story<EnfoldProps> = (args) => <Enfold {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  
+  children: <div>asd</div>,
+  name: ""
 };
