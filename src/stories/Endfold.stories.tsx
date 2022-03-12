574481cd2b483edf904ec2430658248ed4ab2672
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-12 21:01:51 +0800

    add enfold story

---
+++ src/stories/Endfold.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+
+import Enfold from "../components/Enfold";
+import { EnfoldProps } from "../components/Enfold.types";
+
+export default {
+  title: "Mart/Enfold",
+  component: Enfold,
+  argTypes: {},
+} as Meta<typeof Enfold>;
+
+const Template: Story<EnfoldProps> = (args) => <Enfold {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  
+};
