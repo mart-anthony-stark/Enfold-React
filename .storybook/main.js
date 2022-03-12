d680ace8467c03f92967bb1e5b69da951de023cb
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-12 20:52:24 +0800

    change config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,7 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    "../src/**/**/*.stories.mdx",
+    "../src/**/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react"
-}
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+};
